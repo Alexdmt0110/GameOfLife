cfd3deeddcc0170f4f9a8108df2e12b9fb9cb9f9
AlexxDmt <alex.deriemont@gmail.com>
2025-10-13 13:14:32 +0200

    Tests on the Game rules

--- Tests/obj/Debug/net9.0/Tests.AssemblyInfo.cs
+++ Tests/obj/Debug/net9.0/Tests.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6bd5646874f93b4284febfd3d2c412edb7ca675c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e77bdf233750d40b9a04cd52b3dc468a31e4c788")]
 [assembly: System.Reflection.AssemblyProductAttribute("Tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
